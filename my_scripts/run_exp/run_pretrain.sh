eca82fd0ccc69aba0487437ba3d318c5436433b8
Yasir Ghunaim <yasirghunaim@gmail.com>
2025-08-04 18:25:51 +0300

    Update checkpoint

--- my_scripts/run_exp/run_pretrain.sh
+++ my_scripts/run_exp/run_pretrain.sh
@@ -4,10 +4,11 @@
 #SBATCH -o output_pretrain/gpu.%A.out
 #SBATCH -e output_pretrain/gpu.%A.err
 #SBATCH --mail-type=FAIL
-#SBATCH --time=30:00:00
-#SBATCH --gres=gpu:a100:1
-#SBATCH --mem=100G
-#SBATCH --cpus-per-task=6
+#SBATCH --time=15:00:00
+#SBATCH --gres=gpu:v100:2
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=12
+#SBATCH --account conf-aaai-2025.08.04-ghanembs
 
 #  
 #####
@@ -17,13 +18,17 @@ conda activate efficient_atom_den
 ##### 
 
 cd ..
-CUDA_VISIBLE_DEVICES=0 python pretrain.py \
+
+# python pretrain.py \
+export OMP_NUM_THREADS=2
+torchrun --nproc_per_node=2 pretrain.py \
     --lr 1.0e-4 \
     --batch_size 20 \
     --num_workers 6 \
     --train_samples_limit 1000000 \
     --val_samples_limit 2000 \
-    --task "oc22" \
+    --task "transition1x" \
     --epochs 5 \
     --sampling_strategy "random" \
-    --enable_wandb \
+    --model_name "equiformer_v2" \
+    --enable_wandb 
